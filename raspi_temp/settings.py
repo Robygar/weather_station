71bb65a9ce48366237569eef8864ccfe26adaa5c
Robygar <146089595+Robygar@users.noreply.github.com>
2023-11-14 23:53:31 +0100

    Update settings.py

--- raspi_temp/settings.py
+++ raspi_temp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f708674b-c142-45a6-9120-68abba092c0e'
+SECRET_KEY = 'f708674b-c142-45a6-9987-68abba092c0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
