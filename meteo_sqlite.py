cc1be9d9a6c1eb5513c9443f289b9c2dab04fd54
Robygar <146089595+Robygar@users.noreply.github.com>
2023-11-15 11:58:32 +0100

    Update meteo_sqlite.py
    add comments

--- meteo_sqlite.py
+++ meteo_sqlite.py
@@ -3,19 +3,19 @@ import sqlite3
 import time
 
 DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 3
-COMPENSATION_TEMP = 0
-COMPENSATION_HUMI= 0
+DHT_PIN = 3  #dht sensor pin
+COMPENSATION_TEMP = 0  #modify to calibrate temperature
+COMPENSATION_HUMI= 0 #modify to calibrate humidity
 
 con = sqlite3.connect("meteo.db")
 
 while True:
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     hygr, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    hygr = hygr + COMPENSATION_HUMI  # compenso imprecisione sensore umidità
-    temp = temp + COMPENSATION_TEMP  # compenso imprecisione sensore temperatura
+    hygr = hygr + COMPENSATION_HUMI 
+    temp = temp + COMPENSATION_TEMP 
     cursore = con.cursor()
     cursore.execute("INSERT INTO raspi_app_weather(temp, hygr, date) VALUES (?, ?, ?)", (temp, hygr, date))
     con.commit()
     print(cursore.rowcount)
-    time.sleep(900)
+    time.sleep(900) # 900 seconds 
