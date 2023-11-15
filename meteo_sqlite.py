5e154446a9eb9fab6352f9d352843573aaf4b6cf
Robygar <146089595+Robygar@users.noreply.github.com>
2023-11-15 11:54:42 +0100

    Update meteo_sqlite.py
    variables to compensate for temperature and humidity

--- meteo_sqlite.py
+++ meteo_sqlite.py
@@ -4,14 +4,16 @@ import time
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 3
+COMPENSATION_TEMP = 0
+COMPENSATION_HUMI= 0
 
 con = sqlite3.connect("meteo.db")
 
 while True:
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     hygr, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    hygr = hygr + 20.0  # compenso imprecisione sensore umidità
-    temp = temp + 3.0  # compenso imprecisione sensore temperatura
+    hygr = hygr + COMPENSATION_HUMI  # compenso imprecisione sensore umidità
+    temp = temp + COMPENSATION_TEMP  # compenso imprecisione sensore temperatura
     cursore = con.cursor()
     cursore.execute("INSERT INTO raspi_app_weather(temp, hygr, date) VALUES (?, ?, ?)", (temp, hygr, date))
     con.commit()
